5d94b114021e85dbf2d4fdc332d19d68a0f7a894
Robert S Ciaccio <losmuertos@gmail.com>
2011-05-24 20:13:24 -0400

    added pygame stuff for drawing a pygame/sdl screen

--- grail.py
+++ grail.py
@@ -1,9 +1,39 @@
 import os
 
+import pygame
+#import pygame.locals
+from pygame.locals import *
 
 def main():
+    screen = pygame.display.set_mode((1024, 768))
+    '''
+    You can call set_mode during your game to switch from windowed 
+        (the default) to full-screen. Other display mode flags (you just | 
+        them together):
+    DOUBLEBUF must be used for smooth animation
+    OPENGL lets you draw 3d scenes with PyOpenGL, but won't let you perform
+        most pygame drawing functions.
+    There is an optional bit depth flag, but it's almost always better to 
+        not include it and go with whatever platform default is available.
+    If you're using DOUBLEBUF then you'll need to flip the screen after 
+        you've rendered it. This is as simple as:
+
+        >>> pygame.display.flip()
+    '''    
+    #screen = pygame.display.set_mode((1024, 768), WINDOWED)
+    #screen = pygame.display.set_mode((1024, 768), WINDOWED | DOUBLEBUF)
+    img = pygame.image.load('resources/Car_6a.png')
+    xpos = 50
+    ypos = 100
+    screen.blit(img, (xpos, ypos))
+    redraw()
     stillPlaying = True
     while (stillPlaying):
+        #blank the screen
+        screen.fill((0,0,0))
+        xpos += 50
+        screen.blit(img, (xpos, ypos))
+        redraw()
         answer = raw_input("Start a new game? (y/n): ")
         if answer == 'y' or answer == 'Y':
             startGame()
@@ -14,6 +44,8 @@ def main():
 
     print("Later!")
 
+def redraw():
+    pygame.display.flip()
 
 def evaluateHand(cards):
     return sum(cards)
